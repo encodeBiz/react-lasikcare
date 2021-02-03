4350ce2afdec9dc41955abaf8f0737204533d9da
joseacemedina <joseacemedina@gmail.com>
2021-02-03 15:37:33 +0100

    add services

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import logo from './logo.svg';
 import './styles/App.css';
-
+import { getClinicas, getHuecos } from "./services/appointments.service";
 function App() {
+  getClinicas()
+  .then(res => {
+    console.log('getClinicas', res)
+  })
+  .then(() => {
+    const params = {
+			keycli: 'GR021',
+			date: new Date().toLocaleDateString(),
+			type: 'BI'
+    };
+    
+    getHuecos(params)
+    .then(res => {
+      console.log('getHuecos', res)
+    })
+  })
+
   return (
     <div className="App">
       <header className="App-header">
-        
+        Hola mundo
       </header>
     </div>
   );
---
+++ src/enpoints/enpoints.constants.js
@@ -0,0 +1,2 @@
+export const get_clinicas = 'http://localhost:8888/termine-ws-v2/api/getClinicas.php'
+export const get_huecos = 'http://localhost:8888/termine-ws-v2/api/getHuecos.php'
