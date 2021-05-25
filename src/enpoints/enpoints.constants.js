3c78dff710d2ad41a6bc5d59081f228e6f338f9f
Angel Hernandez <angel@rload.es>
2021-05-25 10:55:22 +0200

    Api calls fixed

--- src/enpoints/enpoints.constants.js
+++ src/enpoints/enpoints.constants.js
@@ -1,3 +1,13 @@
-export const get_clinicas = 'http://localhost/termine-ws-v2/api/getClinicas.php'
-export const get_huecos = 'http://localhost/termine-ws-v2/api/getHuecos.php'
-export const set_huecos = 'http://localhost/termine-ws-v2/api/setHuecos.php'
+/* 
+  export const get_clinicas = 'http://localhost/termine-ws-v2/api/getClinicas.php'
+  export const get_huecos = 'http://localhost/termine-ws-v2/api/getHuecos.php'
+  export const set_huecos = 'http://localhost/termine-ws-v2/api/setHuecos.php' */
+
+//   export const get_clinicas = 'https://www.lasikcare.de/wp-content/plugins/react-wp/api/getClinicas.php'
+//   export const get_huecos = 'https://www.lasikcare.de/wp-content/plugins/react-wp/api/getHuecos.php'
+//   export const set_huecos = 'https://www.lasikcare.de/wp-content/plugins/react-wp/api/setHuecos.php'
+
+  export const get_clinicas = 'http://localhost/termine-ws-v2/api/getClinicas.php'
+  export const get_huecos = 'http://localhost/termine-ws-v2/api/getHuecos.php'
+  export const set_huecos = 'http://localhost/termine-ws-v2/api/setHuecos.php'
+  
