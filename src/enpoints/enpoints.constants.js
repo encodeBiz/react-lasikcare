e5db729ba690b7ef80ce3c93959323c5f3788dd6
joseacemedina <joseacemedina@gmail.com>
2021-06-16 09:55:54 +0200

    update endpoints

--- src/enpoints/enpoints.constants.js
+++ src/enpoints/enpoints.constants.js
@@ -4,7 +4,7 @@
 export const get_clinicas = 'https://www.lasikcare.de/termine-ws-v2/api_react/getClinicas.php'
 export const get_huecos = 'https://www.lasikcare.de/termine-ws-v2/api_react/getHuecos.php'
 export const set_huecos = 'https://www.lasikcare.de/termine-ws-v2/api_react/setHuecos.php' 
-export const send_error_email = "http://www.lasikcare.de/termine-ws-v2/api_react/sendMailError.php"
+export const send_error_email = "https://www.lasikcare.de/termine-ws-v2/api_react/sendMailError.php"
 ///TEST LOCAL///Test
 // export const get_clinicas = 'http://localhost:8888/termine-ws-v2/api/getClinicas.php'
 // export const get_huecos = 'http://localhost:8888/termine-ws-v2/api/getHuecos.php'
