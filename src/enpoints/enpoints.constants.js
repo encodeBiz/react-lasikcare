1916b890c23e1b07dd6aec55bf32d055b226f036
joseacemedina <joseacemedina@gmail.com>
2021-11-29 12:08:15 +0100

    update

--- src/enpoints/enpoints.constants.js
+++ src/enpoints/enpoints.constants.js
@@ -6,7 +6,7 @@ lkcare/wp-includes/termine-ws-v2
 //   export const get_huecos = 'https://www.lasikcare.de/wp-content/plugins/react-wp/api/getHuecos.php'
 //   export const set_huecos = 'https://www.lasikcare.de/wp-content/plugins/react-wp/api/setHuecos.php'
   */
-    export const get_clinicas = 'http://localhost:8888/termine-ws-v2/api/getClinicas.php'
-  export const get_huecos = 'http://localhost:8888/termine-ws-v2/api/getHuecos.php'
-  export const set_huecos = 'http://localhost:8888/termine-ws-v2/api/setHuecos.php'
+export const get_clinicas = 'http://localhost:8888/termine-ws-v2/api/getClinicas.php'
+export const get_huecos = 'http://localhost:8888/termine-ws-v2/api/getHuecos.php'
+export const set_huecos = 'http://localhost:8888/termine-ws-v2/api/setHuecos.php'
 
